8a9ff505ae6cbb629261b03054da2d76b7d09e6c
Thorbjoer Wulf <thorwulf@hotmail.com>
2019-05-16 00:25:07 +0200

    Risika-import

--- dataimport/importRisika.py
+++ dataimport/importRisika.py
@@ -15,7 +15,13 @@ def main():
     print("ok")
     db = connect_db('virk')
 
-    #handle csv
+    #   handle csv
+    #   cvrnr;start_date;end_date;bruttoresultat;aaretsresultat;aktiver;egenkapital;kortgaeld;langgaeld;hensaettelser
+    #   sample line: 25789822;01/01/16;31/12/16;88.750;75.666;227.354;155.666;71.688;;
+    #   TODO:
+    #   convert date to year, eg 01/01/16 to 2016
+    #   remove any dots, eg 75.666
+
     cvrlist = []
     with open(csvfile) as fp:
         csv_reader = csv.reader(fp, delimiter=';')
@@ -32,7 +38,11 @@ def main():
                 res = cur.fetchone()
                 if (res is not None):
                     print("ok")
+                    # THIS IS NOT DONE YET
+                    #cur.execute("INSERT INTO audits (cvrnr,companyName,companyID, GrossResult, Equity, LongTermDebt) \
+                    #VALUES ('{}','{}','{}')    ")
                     print(res)
+                    
                     print('; '.join(row))
                 else:
                     print("basd")
